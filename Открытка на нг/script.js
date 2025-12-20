ffa6caeae18e63ca08faaae54888b728a2647142
lauraaabk <bekeshovalaura26@gmail.com>
2025-12-20 09:47:13 +0500

    add project

---
+++ Открытка на нг/script.js
@@ -0,0 +1,9 @@
+function openCard() {
+	let cover = document.getElementById('cover');
+	cover.className = 'open-card';
+}
+
+function closeCard() {
+	let cover = document.getElementById('cover');
+	cover.className = '';
+}
